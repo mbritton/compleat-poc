d0cdf04cfb3a29724f157cf321b06ec05a6763c2
Mike Britton <mbritton72@gmail.com>
2022-10-16 04:08:31 -0400

    Close icon on right overlay

--- components/Hero.js
+++ components/Hero.js
@@ -1,9 +1,14 @@
 import styles from '@/styles/Hero.module.scss';
+import { BsXLg } from 'react-icons/bs';
 
 const Hero = ({ children }) => {
   return (
     <div className={styles.hero}>
-      <div className={styles.rightOverlay}></div>
+      <div className={styles.rightOverlay}>
+        <div className={styles.closeIcon}>
+          <BsXLg></BsXLg>
+        </div>
+      </div>
     </div>
   );
 };
