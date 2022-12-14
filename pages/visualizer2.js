652ae260f8d6ad3d41cddfd67329b4e3c068a2dd
Mike Britton <mbritton72@gmail.com>
2022-12-13 23:39:37 -0500

    x3d stuff

--- pages/visualizer2.js
+++ pages/visualizer2.js
@@ -4,6 +4,10 @@ import { useCallback, useEffect, useRef, useState } from 'react';
 import * as THREE from 'three';
 import { x3dLoader, x3dParser } from 'three-x3d-loader';
 import * as renderX3D from '@/utils/renderX3D';
+import { Object3D } from 'three';
+import { parse } from 'path';
+
+THREE.Cache.enabled = true;
 
 export default function Visualizer2() {
   let mountRef = useRef();
@@ -21,48 +25,73 @@ export default function Visualizer2() {
   );
   let xmlDOM;
   const uri3D =
-    'https://prismic-io.s3.amazonaws.com/compleat/41b785a8-fad1-4b3e-9127-688b8490be93_3dspiral-position-000.x3d';
+    'https://prismic-io.s3.amazonaws.com/compleat/aa5b53fb-91f9-4aa8-9ab7-e40beac0e77e_3dspiral-position-000.x3d';
 
-  const convertModel = (xmlString) => {
-    const parser = new DOMParser();
-    return parser.parseFromString(xmlString, 'text/xml');
-  };
-
-  const loadModel = useCallback(async (xmlData) => {
+  const parseModel = useCallback(async () => {
     let loader = new THREE.FileLoader(THREE.DefaultLoadingManager);
+    loader.setResponseType('text');
+    const parser = new DOMParser();
     await loader.load(
-      xmlData,
+      uri3D,
       (text) => {
-        xmlDOM = convertModel(text);
-        // scene.add(xmlDOM.body);
-
-        scene.background = new THREE.Color(0xf2f5ff);
-
-        ambientLight = new THREE.AmbientLight(0x404040); // soft white light
-        scene.add(ambientLight);
-
-        const light = new THREE.HemisphereLight(0x404040, 0xffffff, 1);
-        scene.add(light);
-
-        camera.position.z = -30;
-        camera.position.x = 1;
-        camera.position.y = 0;
-        camera.aspect = clientWidth / clientHeight;
-        camera.lookAt(new THREE.Vector3(0, 0, 0));
-        camera.updateProjectionMatrix();
-
-        const geometry = new THREE.BoxGeometry(1, 1, 1);
-        const material = new THREE.MeshBasicMaterial({ color: '#433F81' });
-        let cube = new THREE.Mesh(geometry, material);
-        scene.add(cube);
+        const foo = parser.parseFromString(text, 'text/xml');
+        renderModel(foo);
       },
-      () => {
-        console.log('progress');
-      },
-      (e) => {
-        console.log('error', e);
+      (progress) => {
+        console.log('progress', progress);
       },
     );
+  }, []);
+
+  const renderModel = useCallback((foo) => {
+    console.log('renderModel', foo);
+
+    scene.background = new THREE.Color(0xf2f5ff);
+
+    ambientLight = new THREE.AmbientLight(0x404040); // soft white light
+    scene.add(ambientLight);
+
+    const light = new THREE.HemisphereLight(0x404040, 0xffffff, 1);
+    scene.add(light);
+
+    camera.position.z = -30;
+    camera.position.x = 1;
+    camera.position.y = 0;
+    camera.aspect = clientWidth / clientHeight;
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+    camera.updateProjectionMatrix();
+
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: '#433F81' });
+    let cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    renderer.render(scene, camera);
+  }, []);
+
+  const loadModel = useCallback(async (xmlData) => {
+    await parseModel().then((xmlDOMRet) => {
+      // console.log('xmlDOMRet', xmlDOMRet);
+      // xmlDOM = parser.parseFromString(text, 'text/xml');
+      // const xmlDomObj = parseModel(xmlDOM);
+      // renderX3D(THREE, xmlDOMRet, scene, material);
+      // renderer.render(scene, camera);
+      // scene.background = new THREE.Color(0xf2f5ff);
+      // ambientLight = new THREE.AmbientLight(0x404040); // soft white light
+      // scene.add(ambientLight);
+      // const light = new THREE.HemisphereLight(0x404040, 0xffffff, 1);
+      // scene.add(light);
+      // camera.position.z = -30;
+      // camera.position.x = 1;
+      // camera.position.y = 0;
+      // camera.aspect = clientWidth / clientHeight;
+      // camera.lookAt(new THREE.Vector3(0, 0, 0));
+      // camera.updateProjectionMatrix();
+      // const geometry = new THREE.BoxGeometry(1, 1, 1);
+      // const material = new THREE.MeshBasicMaterial({ color: '#433F81' });
+      // let cube = new THREE.Mesh(geometry, material);
+      // scene.add(cube);
+    });
   }, []);
 
   const onWindowResize = useCallback(() => {
@@ -74,10 +103,10 @@ export default function Visualizer2() {
 
     loadModel(uri3D).then(() => {
       onWindowResize();
-      setTimeout(() => {
-        renderX3D(THREE, xmlDOM, scene, material);
-        renderer.render(scene, camera);
-      }, 1000);
+      // setTimeout(() => {
+      //   // renderX3D(THREE, xmlDOM, scene, material);
+      //   // renderer.render(scene, camera);
+      // }, 1000);
     });
 
     return () => {
